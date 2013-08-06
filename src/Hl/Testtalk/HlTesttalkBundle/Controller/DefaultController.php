831f9e7484a4a6238c23186836525cbbc7a69188
Liang He <liang.he@digitalpost.com.au>
2013-08-06 14:25:21 +1000

    Modified

--- src/Hl/Testtalk/HlTesttalkBundle/Controller/DefaultController.php
+++ src/Hl/Testtalk/HlTesttalkBundle/Controller/DefaultController.php
@@ -14,22 +14,19 @@ class DefaultController extends Controller {
         $session = $this->getRequest()->getSession();
         if ($session->has('books')) {
             $books = $session->get('books');
-
-            
         } else {
             $em = $this->getDoctrine()->getManager();
             $books = $em->getRepository('HlTesttalkHlTesttalkBundle:Book')
                     ->findAllBooks();
             $session->set('books', $books);
-            
         }
         $name = $this->getUser()->getUsername();
-
+        $roles = $this->getUser()->getRoles();
 
 //        $book->setImage('default.jpg');
         $this->form = $this->getForm();
         return $this->render(
-                        'HlTesttalkHlTesttalkBundle:Default:index.html.twig', array('name' => $name, 'books' => $books, 'form' => $this->form->createView())
+        'HlTesttalkHlTesttalkBundle:Default:index.html.twig', array('name' => $name, 'role' => $roles[0], 'books' => $books, 'form' => $this->form->createView())
         );
     }
 
@@ -50,9 +47,8 @@ class DefaultController extends Controller {
             unset($books[$found]);
             $session->set('books', $books);
         }
-        
+
         return $this->redirect($this->generateUrl('_testtalk_home'));
-       
     }
 
     public function addAction() {
@@ -62,7 +58,12 @@ class DefaultController extends Controller {
         $form = $request->request->get('form');
         $language = $form['language'];
         $name = $form['name'];
-        $description = 'It is a new book';
+        $description = $form['desc'];
+        if (isset($form['new'])) {
+            $new = $form['new'];
+        } else {
+            $new = false;
+        }
         $bookid = '99999';
         $new_book = new Book();
         $new_book->setDescription($description);
@@ -70,6 +71,7 @@ class DefaultController extends Controller {
         $new_book->setName($name);
         $new_book->setLanguage($language);
         $new_book->setImage('the-guts.jpg');
+        $new_book->setNew($new);
         $books [] = $new_book;
         $session->set('books', $books);
         $html = $this->renderView(
@@ -87,11 +89,12 @@ class DefaultController extends Controller {
         return $this->createFormBuilder()
                         ->setAction($this->generateUrl('_testtalk_add_book'))
                         ->add('name', 'text')
+                        ->add('desc', 'textarea', array('label' => 'Description'))
                         ->add('language', 'choice', array(
                             'choices' => array('English' => 'English', 'Spanish' => 'Spanish', "Russian" => "Russian"),
                             'required' => true,
                         ))
-                        ->add('new', 'checkbox')
+                        ->add('new', 'checkbox', array('required' => false))
                         ->add('Add', 'submit')
                         ->getForm();
     }
